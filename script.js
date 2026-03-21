a6215e4c0f0fd7f599a760eb2f5beb0ddd57c815
Vishank Shukla <vishankshukla51@gmail.com>
2026-03-21 15:30:18 +0530

    New Feature Clear Completed Task

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoInput = document.getElementById("todo-input");
   const addTaskButton = document.getElementById("add-task-btn");
   const todoList = document.getElementById("todo-list");
+  const clearCompletedBtn = document.getElementById("clear-completed-btn");
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach((task) => renderTask(task));
+  toggleClearButton();
 
   addTaskButton.addEventListener("click", () => {
     let taskText = todoInput.value.trim();
@@ -19,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tasks.push(newTask);
     saveTask();
     renderTask(newTask);
+    toggleClearButton();
     todoInput.value = "";
     console.log(tasks);
   });
@@ -35,13 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     if (task.completed) li.classList.add("completed");
     li.innerHTML = `
     <span class="task-text">${task.text}</span>
-    <button id="delete-btn">Delete</button>
+    <button class="delete-btn">Delete</button>
     `;
     li.addEventListener("click", (e) => {
       if (e.target.tagName === "BUTTON") return;
       task.completed = !task.completed;
       li.classList.toggle("completed");
       saveTask();
+      toggleClearButton();
     });
 
     li.querySelector("button").addEventListener("click", (e) => {
@@ -49,6 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
       tasks = tasks.filter((t) => t.id !== task.id);
       li.remove();
       saveTask();
+      toggleClearButton();
     });
     todoList.appendChild(li);
   }
@@ -56,4 +61,24 @@ document.addEventListener("DOMContentLoaded", () => {
   function saveTask() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
+
+function toggleClearButton() {
+  const hasCompleted = tasks.some(task => task.completed);
+
+  if (hasCompleted) {
+    clearCompletedBtn.style.display = "block";
+  } else {
+    clearCompletedBtn.style.display = "none";
+  }
+}
+
+clearCompletedBtn.addEventListener("click", () => {
+  tasks = tasks.filter(task => !task.completed);
+  saveTask();
+
+  todoList.innerHTML = "";
+  tasks.forEach(task => renderTask(task));
+
+  toggleClearButton();
+});
 });
